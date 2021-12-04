47c44c5755654eaab60513eb1a8a7f3c21a89946
Gontier Julien <gontierjulien68@gmail.com>
2021-12-04 20:39:34 +0100

    Update build.yml
    Signed-off-by: Gontier Julien <gontierjulien68@gmail.com>

---
+++ build.sh
@@ -0,0 +1,9 @@
+ #!/usr/bin/env bash
+files=$(find . -name "PKGBUILD")
+
+for f in $files
+do
+  d=$(dirname $f)
+  cd $d
+
+  docker run --name dockerbuild -e EXPORT_PKG=1 -v $PWD:/pkg whynothugo/makepkg
