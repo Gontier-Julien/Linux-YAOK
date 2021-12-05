e7cfb3a1448245a5ad6fa395daa16e5710e1e86e
Julien Gontier <52894530+Gontier-Julien@users.noreply.github.com>
2021-12-05 01:53:06 +0100

    This should fix build problem

--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@ files=$(find . -name "PKGBUILD")
 for f in $files
 do
   d=$(dirname $f)
+  cd ../
   cd $d
 
-  #always remove old docker build
-  docker rm $(docker ps -aq)
   docker run --name dockerbuild -e EXPORT_PKG=1 -v $PWD:/pkg snowy68/makepkg
 done
