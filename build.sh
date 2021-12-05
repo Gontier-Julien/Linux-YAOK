7ddc91afe9669b22dbc28fc8fbd78566edfa1472
Gontier Julien <gontierjulien68@gmail.com>
2021-12-05 20:36:30 +0100

    Maybe this should fix it
    Signed-off-by: Gontier Julien <gontierjulien68@gmail.com>

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ do
   cd $d
 
   docker run --name dockerbuild -e EXPORT_PKG=1 -v $PWD:/pkg snowy68/makepkg
-  sudo docker cp dockerbuild:/pkg/*/*.zst /home/runner/work/Linux-YAOK/Linux-YAOK/
   docker rm dockerbuild
   cd ..
 done
+
+mv */*.tar.zst* /home/runner/work/Linux-YAOK/Linux-YAOK/
